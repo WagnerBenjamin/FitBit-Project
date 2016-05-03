bbd080c37a4ab246dea9b3bd91e2ecc980fc13db
Antoine <becherantoine@gmail.com>
2016-05-03 16:27:37 +0200

    Ajouts de pages Signup/Login/Account

---
+++ ourApp/www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('myAccountCtrl', function($scope) {
+
+})
+ 
