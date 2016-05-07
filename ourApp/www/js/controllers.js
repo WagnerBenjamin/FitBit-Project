587c6349491b1e51cf1ba3349bbed691bcaa76ac
Antoine <becherantoine@gmail.com>
2016-05-08 01:27:56 +0200

    Modifs

--- ourApp/www/js/controllers.js
+++ ourApp/www/js/controllers.js
@@ -1,18 +1,17 @@
 angular.module('app.controllers', [])
-  
-.controller('profileCtrl', function($scope) {
 
+.controller('profileCtrl', function($scope) {
+  
 })
-   
+
 .controller('activityCtrl', function($scope) {
 
 })
-   
+
 .controller('performanceCtrl', function($scope) {
 
 })
-      
+
 .controller('sleepCtrl', function($scope) {
 
-})
- 
+});
--- ourApp/www/js/directives.js
+++ ourApp/www/js/directives.js
@@ -4,3 +4,7 @@ angular.module('app.directives', [])
 
 }]);
 
+angular.module("myapp", ["chart.js"]).controller("DoughnutCtrl", function ($scope) {
+  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.data = [300, 500, 100];
+});
