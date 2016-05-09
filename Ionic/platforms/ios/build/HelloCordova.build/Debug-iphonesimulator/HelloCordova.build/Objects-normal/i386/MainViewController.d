a2a98d083589c927bbb95bce35b6b3b191f20c4a
Antoine <becherantoine@gmail.com>
2016-05-09 18:59:41 +0200

    Remove platforms

---
+++ Ionic/platforms/ios/build/HelloCordova.build/Debug-iphonesimulator/HelloCordova.build/Objects-normal/i386/MainViewController.d
@@ -0,0 +1,15 @@
+dependencies: \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/HelloCordova/Classes/MainViewController.m \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/HelloCordova/Classes/MainViewController.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVViewController.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVAvailability.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVAvailabilityDeprecated.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVInvokedUrlCommand.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVCommandDelegate.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVCommandQueue.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVScreenOrientationDelegate.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVPlugin.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVPluginResult.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/NSMutableArray+QueueAdditions.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVWebViewEngineProtocol.h \
+  /Users/antoinebecher/Desktop/FitBit-Project/Ionic/platforms/ios/build/emulator/include/Cordova/CDVCommandDelegateImpl.h
