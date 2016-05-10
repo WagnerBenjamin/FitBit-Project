fe00c70efd5137f79c2f1d781e2ace3efa8f61fb
benjamin wagner <benjamin wagner>
2016-05-11 00:02:48 +0200

    Debut Perf

--- api/src/ApiBundle/Entity/DatasetsRepository.php
+++ api/src/ApiBundle/Entity/DatasetsRepository.php
@@ -44,4 +44,46 @@ class DatasetsRepository extends EntityRepository
 
         return $qb->getQuery()->getResult();
     }
+
+    public function getPerfDefault(){
+        $qb = $this->_em->createQueryBuilder()
+            ->select('d.date, d.steps, d.floors, d.distance, d.calories')
+            ->from($this->_entityName, 'd')
+            ->where("d.date BETWEEN DATE_SUB(:end, 6, 'day') AND :end")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getPerfLMonth(){
+        $qb = $this->_em->createQueryBuilder()
+            ->select('avg(d.steps), avg(d.floors), avg(d.distance), avg(d.calories)')
+            ->from($this->_entityName, 'd')
+            ->where("d.date BETWEEN DATE_SUB(:end, 1, 'MONTH') AND :end")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getPerfLYear(){
+        $qb = $this->_em->createQueryBuilder()
+            ->select('avg(d.steps), avg(d.floors), avg(d.distance), avg(d.calories)')
+            ->from($this->_entityName, 'd')
+            ->where("d.date BETWEEN DATE_SUB(:end, 12, 'MONTH') AND :end")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getPerfAllYear(){
+        $qb = $this->_em->createQueryBuilder()
+            ->select('d.date, avg(d.steps), avg(d.floors), avg(d.distance), avg(d.calories)')
+            ->from($this->_entityName, 'd')
+            ->orderBy('d.id');
+
+        return $qb->getQuery()->getResult();
+    }
 }
