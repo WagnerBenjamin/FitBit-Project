aa8c07e23f244cba3eef84e50d0ea2ac1b3b3fb5
mrwaagner <benj.wagner@outlook.com>
2016-05-12 16:39:11 +0200

    better code profile

--- api/src/ApiBundle/Entity/DatasetsRepository.php
+++ api/src/ApiBundle/Entity/DatasetsRepository.php
@@ -20,16 +20,30 @@ class DatasetsRepository extends EntityRepository
     public function getWeightHistLastWeek()
     {
         $qb = $this->_em->createQueryBuilder()
-            ->select('d.date, d.weight')
+            ->select('SUBSTRING(d.date, 1, 10) as date, d.weight')
             ->from($this->_entityName, 'd')
             ->where("d.date BETWEEN DATE_SUB(:end, 6, 'day') AND :end")
             ->setParameter('end', date($this->now))
-            ->orderBy('d.id');
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
 
-        return $qb->getQuery()->getResult();
+        $qq = $this->_em->createQueryBuilder()
+            ->select('SUBSTRING(d.date, 1, 10) as date, d.bmi')
+            ->from($this->_entityName, 'd')
+            ->where("d.date LIKE :end")
+            ->setParameter('end', date($this->now))
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
+
+        $data = array(
+            "weight_hist" => $qb,
+            "bmi_actual" => $qq,
+        );
+
+        return $data;
     }
 
-    public function getBMIActual()
+    /*public function getBMIActual()
     {
         $qb = $this->_em->createQueryBuilder()
             ->select('d.date, d.bmi')
@@ -39,47 +53,99 @@ class DatasetsRepository extends EntityRepository
             ->orderBy('d.id');
 
         return $qb->getQuery()->getResult();
-    }
+    }*/
 
     public function getWeightHistLastMonth()
     {
-        $parsed_date = date_parse($this->now);
-        $year = $parsed_date["year"];
-        $month = $parsed_date["month"];
-        if ($month < 10) $month = '0' . $month;
-
         $qb = $this->_em->createQueryBuilder()
-            ->select('d.date, d.weight')
+            ->select('SUBSTRING(d.date, 1, 10) as date, d.weight')
             ->from($this->_entityName, 'd')
-            ->where("d.date LIKE :month")
-            ->setParameter('month', $year . '-' . $month . '-%')
-            ->orderBy('d.id');
+            ->where("d.date BETWEEN DATE_SUB(:end, 1, 'MONTH')+1 AND :end")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
 
-        return $qb->getQuery()->getResult();
+        $data = array(
+            "weight_hist" => $qb
+        );
+        return $data;
     }
 
     public function getWeightHistLastYear()
     {
-        $parsed_date = date_parse($this->now);
-        $year = $parsed_date["year"];
-        $qb = $this->_em->createQueryBuilder()
-            ->select("d.date, d.weight")
+        $m1 = $this->_em->createQueryBuilder()
+            ->select('SUBSTRING(d.date, 1, 7) as date, d.weight')
             ->from($this->_entityName, 'd')
-            ->where("d.date LIKE :year")
-            ->setParameter('year', $year . '-%');
+            ->where("d.date BETWEEN DATE_SUB(:end, 1, 'MONTH')+1 AND :end")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
+        $m2 = $this->_em->createQueryBuilder()
+            ->select('SUBSTRING(d.date, 1, 7) as date, d.weight')
+            ->from($this->_entityName, 'd')
+            ->where("d.date BETWEEN DATE_SUB(:end, 2, 'MONTH')+1 AND DATE_SUB(:end, 1, 'MONTH')")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
+        $m3 = $this->_em->createQueryBuilder()
+            ->select('SUBSTRING(d.date, 1, 7) as date, d.weight')
+            ->from($this->_entityName, 'd')
+            ->where("d.date BETWEEN DATE_SUB(:end, 3, 'MONTH')+1 AND DATE_SUB(:end, 2, 'MONTH')")
+            ->setParameter('end', date('2016-03-31'))
+            ->orderBy('d.id')
+            ->getQuery()->getResult();
 
-        return $qb->getQuery()->getResult();
+        $arr[0] = $m3[0];
+        $arr[1] = $m2[0];
+        $arr[2] = $m1[0];
+
+        $data = array('lastyear' => $arr);
+        return $data;
     }
 
-    public function getMeanWeightYears(){
+    /*public function getMeanWeightYears(){
         return $this->getEntityManager()
             ->createQuery(
                 'SELECT d.date, d.weight FROM ApiBundle:Datasets d ORDER BY d.id ASC'
             )
             ->getResult();
+    }*/
+
+    public function getWeightYear($year){
+        $qb = $this->_em->createQueryBuilder()
+            ->select('SUBSTRING(d.date, 1, 4) as date, avg(d.weight) as weight')
+            ->from($this->_entityName, 'd')
+            ->where("d.date LIKE '$year%'")
+            ->orderBy('d.id');
+
+        return $qb->getQuery()->getResult();
     }
 
-    public function getPerfDefault() //modif
+    public function getMeanWeightYears()
+    {
+        $m1 = $this->getWeightYear("2010");
+        $m2 = $this->getWeightYear("2011");
+        $m3 = $this->getWeightYear("2012");
+        $m4 = $this->getWeightYear("2013");
+        $m5 = $this->getWeightYear("2014");
+        $m6 = $this->getWeightYear("2015");
+        $m7 = $this->getWeightYear("2016");
+
+        $arr[0] = $m1[0];
+        $arr[1] = $m2[0];
+        $arr[2] = $m3[0];
+        $arr[3] = $m4[0];
+        $arr[4] = $m5[0];
+        $arr[5] = $m6[0];
+        $arr[6] = $m7[0];
+
+        $data = array(
+            'Mean_weight_years' => $arr
+        );
+        return $data;
+    }
+
+    public function getPerfDefault()
     {
         $qb = $this->_em->createQueryBuilder()
             ->select('SUBSTRING(d.date, 1, 10) as date, d.steps, d.floors, d.distance, d.calories')
@@ -121,7 +187,7 @@ class DatasetsRepository extends EntityRepository
     public function getPerfLYear()
     {
         $m1 = $this->_em->createQueryBuilder()
-            ->select('SUBSTRING(d.date, 1, 7) as date, d.steps, d.floors, d.distance, d.calories')
+            ->select('SUBSTRING(d.date, 1, 7) as date, avg(d.steps) as steps, avg(d.floors) as floors, avg(d.distance) as distance, avg(d.calories) as calories')
             ->from($this->_entityName, 'd')
             ->where("d.date BETWEEN DATE_SUB(:end, 1, 'MONTH')+1 AND :end")
             ->setParameter('end', date('2016-03-31'))
